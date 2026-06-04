cf001009ccb6e15c40896a1385e390be3d4318a7
destery2k <lavrinenko.serhii@e-u.edu.ua>
2026-06-04 13:12:09 +0300

    Update 9.cpp

--- src/9.cpp
+++ src/9.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_map<int, int> freq;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    int pairs = 0;
+
+    for (auto &p : freq) {
+        pairs += p.second / 2;
+    }
+
+    cout << pairs;
+
+    return 0;
+}
