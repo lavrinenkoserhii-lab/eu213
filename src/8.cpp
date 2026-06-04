45242971a24cb684335bf51844533aef504c92a6
destery2k <lavrinenko.serhii@e-u.edu.ua>
2026-06-04 13:09:20 +0300

    Update 8.cpp

--- src/8.cpp
+++ src/8.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> freq(6, 0);
+
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+
+    int bestType = 1;
+    int bestCount = freq[1];
+
+    for (int i = 2; i <= 5; i++) {
+        if (freq[i] > bestCount) {
+            bestCount = freq[i];
+            bestType = i;
+        }
+    }
+
+    cout << bestType;
+
+    return 0;
+}
