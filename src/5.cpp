31881fd6049e8c83ab4639b295d2b6dacf650893
destery2k <lavrinenko.serhii@e-u.edu.ua>
2026-06-04 12:57:01 +0300

    Update 5.cpp

--- src/5.cpp
+++ src/5.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int x1, v1, x2, v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+
+    if (v1 == v2) {
+        if (x1 == x2) cout << "YES";
+        else cout << "NO";
+        return 0;
+    }
+
+    int num = x2 - x1;
+    int den = v1 - v2;
+
+    if (den != 0 && num % den == 0 && num / den >= 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
+}
