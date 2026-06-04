11058343860d2124c35bcf6b93e57d54a1048bec
destery2k <lavrinenko.serhii@e-u.edu.ua>
2026-06-04 13:05:43 +0300

    Update 7.cpp

--- src/7.cpp
+++ src/7.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> scores(n);
+    for (int i = 0; i < n; i++) {
+        cin >> scores[i];
+    }
+
+    int best = scores[0];
+    int worst = scores[0];
+
+    int bestCount = 0;
+    int worstCount = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (scores[i] > best) {
+            best = scores[i];
+            bestCount++;
+        }
+        if (scores[i] < worst) {
+            worst = scores[i];
+            worstCount++;
+        }
+    }
+
+    cout << bestCount << " " << worstCount;
+
+    return 0;
+}
