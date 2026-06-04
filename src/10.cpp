7cd186b6ce74b5d7b50d121ee3d41fa653ef1f97
destery2k <lavrinenko.serhii@e-u.edu.ua>
2026-06-04 13:14:55 +0300

    Update 10.cpp

--- src/10.cpp
+++ src/10.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int leftDiag = 0;
+    int rightDiag = 0;
+
+    for (int i = 0; i < n; i++) {
+        leftDiag += arr[i][i];
+        rightDiag += arr[i][n - i - 1];
+    }
+
+    cout << abs(leftDiag - rightDiag);
+
+    return 0;
+}
